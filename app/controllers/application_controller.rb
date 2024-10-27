335560efc97252ba4af729858ecb5d71ce70e1f9
Vlad Liubelski <vladislav.sl9696@gmail.com>
2024-10-27 02:47:18 +0100

    US2 Bid Statistics. Add SignIn/Up func/modal. Upd root page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
+  helper_method :current_carrier
+
+  before_action :current_carrier
+  def current_carrier
+    @current_carrier ||= Carrier.find_by(id: cookies[:carrier_id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get 'home/index'
 
   resources :bids
+  post 'sign_in' => 'sign_in#create'
+  delete 'logout' => 'sign_in#destroy'
 end
