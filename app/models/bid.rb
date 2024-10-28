2a51750511e5bd2932ec1cc57c584ae993242fbe
Vlad <vladislav.sl9696@gmail.com>
2024-10-28 15:31:29 +0100

    v3 (#13)
    * DE1 price number_with_precision, validation, form field (#11)
    
    * DE2 Add specs for BidsPresenter (#12)
    
    * DE2 Add specs for BidsPresenter

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -5,6 +5,6 @@ class Bid < ApplicationRecord
   belongs_to :shipping_route
   belongs_to :load_type
 
-  validates :price, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
   validates :carrier_id, uniqueness: { scope: %i[load_type_id shipping_route_id] }
 end
