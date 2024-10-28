48891cd691ed6b96263b5c34f0a0ee98a8e8f539
Vlad <vladislav.sl9696@gmail.com>
2024-10-28 03:18:28 +0100

    US6 setup rspec, db_cleaner, simplecov (#6)

---
+++ spec/services/bids/detail_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe Bids::Detail do
+  subject { described_class.new(bid, lowest_bid) }
+
+  let(:bid) { create(:bid, carrier: carrier, load_type: load_type, shipping_route: shipping_route, price: 1000.00) }
+  let(:lowest_bid) { 2000.00 }
+  let(:carrier) { create(:carrier, company_name: 'test1') }
+  let(:load_type) { create(:load_type, name: '5 pallets') }
+  let(:shipping_route) { create(:shipping_route, origin: 'Berlin', destination: 'Warsaw') }
+
+  it '#route' do
+    expect(subject.route).to eq('Berlin → Warsaw')
+  end
+
+  it '#load_type' do
+    expect(subject.load_type).to eq(load_type.name)
+  end
+
+  it '#price' do
+    expect(subject.price).to eq('1000.0 $')
+  end
+
+  it '#lowest_price' do
+    expect(subject.lowest_price).to eq('2000.0 $')
+  end
+
+  it '#on_win' do
+    expect { |b| subject.on_win(&b) }.to yield_control
+  end
+
+  it '#on_lose' do
+    expect { |b| subject.on_lose(&b) }.not_to yield_control
+  end
+end
